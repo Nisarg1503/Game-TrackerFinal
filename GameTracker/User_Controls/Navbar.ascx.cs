270d37b9e07c8d1a805194c594cdde0c8c0d1b0e
Nisarg Patel <Nisarg.Patel@MyGeorgian.ca>
2016-06-22 20:37:13 -0400

    Navbar Auth check

--- GameTracker/User_Controls/Navbar.ascx.cs
+++ GameTracker/User_Controls/Navbar.ascx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 /**
  * @author: Nisarg Patel
  * @date: June 01, 2016
@@ -17,7 +22,26 @@ namespace GameTracker
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SetActivePage();
+            if (!IsPostBack)
+            {
+                // check if a user is logged in
+                if (HttpContext.Current.User.Identity.IsAuthenticated)
+                {
+
+                    // show the Contoso Content area
+                    GamersAreaPlaceHolder.Visible = true;
+                    PublicPlaceHolder.Visible = false;
+                    
+                }
+                else
+                {
+                    // only show login and register
+                    GamersAreaPlaceHolder.Visible = false;
+                    PublicPlaceHolder.Visible = true;
+                    //UserPlaceHolder.Visible = false;
+                }
+                SetActivePage();
+            }
         }
         /**
          * This method adds a css class of "active" to list items related
--- GameTracker/User_Controls/Navbar.ascx.designer.cs
+++ GameTracker/User_Controls/Navbar.ascx.designer.cs
@@ -31,6 +31,15 @@ namespace GameTracker {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl about;
         
         /// <summary>
+        /// PublicPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder PublicPlaceHolder;
+        
+        /// <summary>
         /// Login control.
         /// </summary>
         /// <remarks>
@@ -38,6 +47,15 @@ namespace GameTracker {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl Login;
+        
+        /// <summary>
+        /// GamersAreaPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder GamersAreaPlaceHolder;
         
         /// <summary>
         /// Games control.
